5d6d72521f159a96e4ca39159c8f80806c11a667
Peter Reid <peter.d.reid@gmail.com>
2015-08-03 13:44:39 -0400

    Added Font::measure_width

--- src/font.rs
+++ src/font.rs
@@ -2,6 +2,7 @@
 use libharu_sys as haru;
 use document_inner::DocumentInner;
 use std::rc::Rc;
+use std::f32;
 
 #[derive(Clone)]
 pub struct Font {
@@ -29,5 +30,23 @@ impl  Font {
     
     pub unsafe fn get_handle(&self) -> haru::HPDF_Page {
         self.handle
+    }
+    
+    pub fn measure_width(&self, text: &str, size: f32) -> f32 {
+        let mut actual_width: f32 = 0.0;
+        unsafe {
+            haru::HPDF_Font_MeasureText(
+                self.handle,
+                text.as_bytes().as_ptr(),
+                text.as_bytes().len() as u32,
+                f32::MAX, // width to fit in
+                size,
+                0.0, // char space
+                0.0, // word space
+                0, // word wrap off
+                &mut actual_width
+            );
+        }
+        actual_width
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -31,15 +31,23 @@ fn simple() {
         try!(page1.line_to(Point::new(100.0, 100.0)));
         try!(page1.fill());
         
-        /*let font = try!(doc.get_ttf_font(File::open("tiny.ttf").unwrap()));
+        /*let font = try!(doc.get_ttf_font(File::open("Roboto-Regular.ttf").unwrap()));
         
+        let to_print = "35° 44.23' with some";
         try!(page1.set_rgb_fill(0.0, 0.0, 0.0));
-        try!(page1.set_font_and_size(font, 14.0));
+        try!(page1.set_font_and_size(&font, 14.0));
         try!(page1.begin_text());
-        try!(page1.text_out(Point::new(40.0, 40.0), "35° 44.23'"));
+        try!(page1.text_out(Point::new(40.0, 40.0), to_print));
         try!(page1.end_text());
-        */
-        let mut out = ::std::fs::File::create("2.pdf").unwrap();
+        
+        let width = font.measure_width(to_print, 14.0);
+        println!("Width = {}", width);
+        
+        try!(page1.move_to(Point::new(40.0, 40.0)));
+        try!(page1.line_to(Point::new(40.0 + width, 40.0)));
+        try!(page1.stroke());*/
+        
+        let mut out = File::create("2.pdf").unwrap();
         try!(doc.save(&mut out));
         
         
@@ -48,6 +56,6 @@ fn simple() {
     }
     
     go().unwrap();
-    assert!(false);
+    panic!("To see printlns");
 }
 
